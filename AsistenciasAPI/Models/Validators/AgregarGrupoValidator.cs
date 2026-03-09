5626586493790c87a07fb5f386b7d3cbbd2490cf
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-09 16:49:42 -0600

    Validators para Editargrupo y Agregargrupo.

---
+++ AsistenciasAPI/Models/Validators/AgregarGrupoValidator.cs
@@ -0,0 +1,23 @@
+﻿using AsistenciasAPI.Models.DTOs;
+using AsistenciasAPI.Models.Entities;
+using AsistenciasAPI.Repositories;
+using FluentValidation;
+
+namespace AsistenciasAPI.Models.Validators
+{
+    public class AgregarGrupoValidator: AbstractValidator<GrupoDTO> 
+    {
+        private readonly Repository<Grupo> repository;
+        public AgregarGrupoValidator(Repository<Grupo> repository) 
+        {
+            this.repository = repository;
+        
+            RuleFor(x => x.Nombre).NotEmpty().WithMessage("Escriba el nombre del grupo")
+                .MaximumLength(20).WithMessage("El grupo no puede tener mas de 20 caracteres de largo")
+                .Must(nombre => !repository.GetAll().Any(g => g.Nombre == nombre))
+                .WithMessage("El nombre del repositorio ya está registrado");
+
+        }
+
+    }
+}
---
+++ AsistenciasAPI/Models/Validators/EditarGrupoValidator.cs
@@ -0,0 +1,29 @@
+﻿using AsistenciasAPI.Models.DTOs;
+using AsistenciasAPI.Models.Entities;
+using AsistenciasAPI.Repositories;
+using FluentValidation;
+
+namespace AsistenciasAPI.Models.Validators
+{
+    public class EditarGrupoValidator:AbstractValidator<EditarGrupoDTO>
+    {
+        private readonly Repository<Grupo> repository;
+        public EditarGrupoValidator(Repository<Grupo> repository)
+        {
+            this.repository = repository;
+
+            RuleFor(x => x.Nombre).NotEmpty().WithMessage("Escriba el nombre del grupo")
+                .MaximumLength(20).WithMessage("El grupo no puede tener mas de 20 caracteres de largo");
+
+            RuleFor(x => x)
+                .Must(ExisteGrupo)
+                .WithMessage("El nombre del repositorio ya está registrado");
+
+        }
+
+        public bool ExisteGrupo(EditarGrupoDTO grupo) 
+        {
+            return !repository.GetAll().Any(x => grupo.Id != x.Id && grupo.Nombre == x.Nombre);
+        }
+    }
+}
