1be1302090eb9aaca96410f0f272814f56cfe105
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-10 16:49:42 -0600

    Services de grupo y algunos metodos del Controllador

---
+++ AsistenciasAPI/Controllers/GrupoControllers.cs
@@ -0,0 +1,53 @@
+﻿using AsistenciasAPI.Models.DTOs;
+using AsistenciasAPI.Services;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens.Experimental;
+
+namespace AsistenciasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrupoControllers : Controller
+    {
+        public GruposService Service { get; }
+        public GrupoControllers(GruposService service)
+        {
+            Service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var dto = Service.GetAll();
+            return Ok(dto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var dto = Service.Get(id);
+            if (dto == null)
+            {
+                return NotFound();
+            }
+            return Ok(dto);
+        }
+
+        [HttpPut]
+        public IActionResult Post(AgregarGrupoDTO dto) 
+        {
+            try
+            {
+                var id = Service.Agregar(dto);
+                return Created("grupo", id); //Cuando no es rest hacer un post que a veces haga esto, a veces otra cosa
+            }
+            catch (ValidationException ex)
+            {
+                var mensajesError = ex.Errors.Select(x => x.ErrorMessage);
+                return BadRequest(mensajesError);
+            }
+        
+        }
+    }
+}
---
+++ AsistenciasAPI/Services/GruposService.cs
@@ -0,0 +1,86 @@
+﻿using AsistenciasAPI.Models.DTOs;
+using AsistenciasAPI.Models.Entities;
+using AsistenciasAPI.Repositories;
+using AutoMapper;
+using FluentValidation;
+
+namespace AsistenciasAPI.Services
+{
+    public class GruposService
+    {
+        private readonly Repository<Grupo> repository;
+        private readonly IMapper mapper;
+        private readonly IValidator<AgregarGrupoDTO> validadorAgregar;
+        private readonly IValidator<EditarGrupoDTO> validadorEditar;
+
+        public GruposService(Repository<Grupo> repository, IMapper mapper, IValidator<AgregarGrupoDTO> validadorAgregar, IValidator<EditarGrupoDTO> validadorEditar)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+            this.validadorAgregar = validadorAgregar;
+            this.validadorEditar = validadorEditar;
+        }
+
+        public List<GrupoDTO> GetAll()
+        {
+            var grupos = repository.GetAll().OrderBy(x => x.Nombre)
+                .Select(mapper.Map<GrupoDTO>)
+                .ToList();
+            return grupos;
+        }
+
+        public GrupoDTO? Get(int idGrupo)
+        {
+            var grupo = repository.Get(idGrupo);
+
+            if (grupo != null)
+            {
+                return mapper.Map<GrupoDTO>(grupo);
+            }
+            return null;
+        }
+
+        public int Agregar(AgregarGrupoDTO dto)
+        {
+            var result = validadorAgregar.Validate(dto);
+            if (result.IsValid)
+            {
+                var entity = mapper.Map<Grupo>(dto);
+                repository.Insert(entity);
+                return entity.Id;
+            }
+            else
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+        public void Editar(EditarGrupoDTO dto)
+        {
+            var result = validadorEditar.Validate(dto);
+            if (result.IsValid)
+            {
+                var entity = mapper.Map<Grupo>(dto);
+                repository.Insert(entity);
+            }
+            else
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+        public bool Eliminar(int idGrupo)
+        {
+            if (repository.Get(idGrupo) != null)
+            {
+                repository.Delete(idGrupo);
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+    }
+}
