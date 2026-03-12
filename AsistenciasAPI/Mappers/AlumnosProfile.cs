8d4d68f04b3834ddc6aac2115860414f523dbadb
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-12 16:52:36 -0600

    Mapper de Asistencias

---
+++ AsistenciasAPI/Mappers/AlumnosProfile.cs
@@ -0,0 +1,27 @@
+﻿using AsistenciasAPI.Models.DTOs;
+using AsistenciasAPI.Models.Entities;
+using AutoMapper;
+
+namespace AsistenciasAPI.Mappers
+{
+    public class AlumnosProfile : Profile
+    {
+        public AlumnosProfile() 
+        {
+            CreateMap<Alumno, AlumnoDTO>()
+                .ForMember(x => x.Asistencia, 
+                y => y.MapFrom((o, d) => {
+                    var estado = o.Asistencia.Where(x => x.Fecha == DateTime.Now)
+                    .Select(x => x.IdEstado).FirstOrDefault();
+
+                    return estado == 1 ? 'a' : estado == 2 ? 'B' : estado == 3 ? 'C';
+                }))
+                .ForMember(x => x.Justificacion, opt => opt.MapFrom((o,d) => {
+                    var motivo = o.Asistencia.Where(x => x.Fecha == DateTime.Now)
+                    .Select(x => x.IdEstado).FirstOrDefault();
+                    return motivo;
+                }
+                ));
+        }
+    }
+}
---
+++ AsistenciasAPI/Models/DTOs/AlumnoDTO.cs
@@ -0,0 +1,37 @@
+﻿namespace AsistenciasAPI.Models.DTOs
+{
+    public class AlumnoDTO
+    {
+        public int Id { set; get; }
+        public string Nombre { get; set; } = null!;
+        public string NumControl { get; set; } = null!;
+        public char Asistencia { get; set; }
+        public string? Justificacion { get; set; }
+    }
+
+    public class AgregarAlumnoDTO 
+    {
+        public string Nombre { get; set; } = null!;
+        public string NumControl { get; set; } = null!;
+    }
+
+    public class AlumnoDetallesDTO
+    {
+        public int Id { set; get; }
+        public string Nombre { get; set; } = null!;
+        public string NumControl { get; set; } = null!;
+    }
+
+    public class EditarAlumnoDTO : AlumnoDetallesDTO { }
+
+    public class AsistenciaDTO 
+    {
+        public string IdAlumno { get; set; } = null!;
+        public DateOnly Fecha { get; set; }
+    }
+
+    public class JustificacionDTO 
+    {
+        public string Motivo { set; get; } = null!;
+    }
+}
