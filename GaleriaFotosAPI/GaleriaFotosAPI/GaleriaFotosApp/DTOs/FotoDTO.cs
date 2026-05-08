b44f165c154d06fab1d6cb2c9c9153145546686d
Ramiro0318 <ramiro.val03@gmail.com>
2026-05-08 17:01:03 -0600

    DTOs creado, service de foto. con metodos de TomarFoto y SubirFoto.

---
+++ GaleriaFotosAPI/GaleriaFotosAPI/GaleriaFotosApp/DTOs/FotoDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriaFotosApp.DTOs
+{
+    public class SubirFotoRequest
+    {
+        public string ImagenBase64 { get; set; } = null!;
+    }
+
+    public class FotoDto
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; } = null!;
+        public string NombreArchivo { get; set; } = null!;
+        public DateTime FechaSubida { get; set; }
+    }
+
+    public class SubirFotoResponse
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ GaleriaFotosAPI/GaleriaFotosAPI/GaleriaFotosApp/Services/FotoService.cs
@@ -0,0 +1,48 @@
+﻿using GaleriaFotosApp.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriaFotosApp.Services
+{
+    public class FotoService
+    {
+        private readonly HttpClient cliente;
+
+        public FotoService(HttpClient cliente)
+        {
+            this.cliente = cliente;
+        }
+
+        public async void TomarFoto()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult? photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    using Stream sourceStream = await photo.OpenReadAsync();
+
+                    byte[] buffer = new byte[sourceStream.Length];
+                    sourceStream.ReadExactly(buffer, 0, buffer.Length);
+
+                    var base64 = Convert.ToBase64String(buffer);
+
+                    await SubirFoto(base64);
+                }
+            }
+        }
+
+        public async Task SubirFoto(string imgBase64)
+        {
+            SubirFotoRequest req = new SubirFotoRequest { 
+                ImagenBase64 = imgBase64,
+            };
+            var response = await cliente.PostAsJsonAsync("/fotos", req);
+        }
+    }
+}
